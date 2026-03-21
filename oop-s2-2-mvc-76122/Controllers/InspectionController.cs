dc74cebcdd9d18544298c486fa8e561f810416aa
Rafaela Omena <rafaomena.s@gmail.com>
2026-03-21 01:49:19 +0000

    Finished Inspection CRUD with EF and MVC

---
+++ oop-s2-2-mvc-76122/Controllers/InspectionController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using oop_s2_2_mvc_76122.Data;
+using Library.Domain;
+
+namespace oop_s2_2_mvc_76122.Controllers
+{
+    public class InspectionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InspectionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var inspections = await _context.Inspections
+                .Include(i => i.Premises)
+                .ToListAsync();
+
+            return View(inspections);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Premises = _context.Premises.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Inspection inspection)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Inspections.Add(inspection);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Premises = _context.Premises.ToList();
+            return View(inspection);
+        }
+    }
+}
