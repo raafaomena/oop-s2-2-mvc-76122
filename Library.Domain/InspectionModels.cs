53709ee7562b6efcf7d4458b31677852e59373b9
Rafaela Omena <rafaomena.s@gmail.com>
2026-03-23 11:23:41 +0000

    Fixed CI location to root directory

--- Library.Domain/InspectionModels.cs
+++ Library.Domain/InspectionModels.cs
@@ -1,46 +1,48 @@
-namespace Library.Domain;
+using System;
+using System.Collections.Generic;
 
-public class Premises
+namespace Library.Domain
 {
-    public int Id { get; set; }
+    public class Premises
+    {
+        public int Id { get; set; }
 
-    public string Name { get; set; } = "";
+        public string Name { get; set; } = "";
 
-    public string Address { get; set; } = "";
+        public string Address { get; set; } = "";
 
-    public string Type { get; set; } = "";
+        public string Type { get; set; } = "";
 
-    public List<Inspection> Inspections { get; set; } = new();
-}
+        public List<Inspection> Inspections { get; set; } = new();
+    }
 
-public class Inspection
-{
-    public int Id { get; set; }
+    public class Inspection
+    {
+        public int Id { get; set; }
 
-    public int PremisesId { get; set; }
+        public int PremisesId { get; set; }
 
-    public Premises? Premises { get; set; }
+        public Premises? Premises { get; set; }
 
-    public DateTime InspectionDate { get; set; }
+        public DateTime InspectionDate { get; set; }
 
-    public string InspectorName { get; set; } = "";
+        public string InspectionName { get; set; } = "";
 
-    public string Result { get; set; } = "";
+        public string Result { get; set; } = "";
 
-    public List<FollowUp> FollowUps { get; set; } = new();
-}
+        public List<FollowUp> FollowUps { get; set; } = new();
+    }
 
-public class FollowUp
-{
-    public int Id { get; set; }
+    public class FollowUp
+    {
+        public int Id { get; set; }
 
-    public int InspectionId { get; set; }
+        public int InspectionId { get; set; }
 
-    public Inspection? Inspection { get; set; }
+        public Inspection? Inspection { get; set; }
 
-    public string ActionRequired { get; set; } = "";
+        public string Notes { get; set; } = "";
 
-    public DateTime DueDate { get; set; }
-
-    public bool IsCompleted { get; set; } = false;
+        public DateTime FollowUpDate { get; set; }
+    }
 }
